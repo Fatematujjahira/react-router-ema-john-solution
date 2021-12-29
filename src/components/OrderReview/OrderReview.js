e9287d5542b80a764b0f898e2d3cd982c14078d8
fatematujjahira <zohrafatema357@gmail.com>
2021-12-29 17:38:28 +0600

    recap

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,47 +1,47 @@
-import React from 'react';
-import useProducts from '../../hooks/useProducts';
-import useCart from '../../hooks/useCart';
-import Cart from '../Cart/Cart';
-import ReviewItem from '../ReviewItem/ReviewItem';
-import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import useProducts from "../../hooks/useProducts";
+import useCart from "../../hooks/useCart";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import { clearTheCart, removeFromDb } from "../../utilities/fakedb";
+import { useHistory } from "react-router-dom";
 
 const OrderReview = () => {
-    const [products] = useProducts();
-    const [cart, setCart] = useCart(products);
-    const history = useHistory();
-    const handleRemove = key => {
-        const newCart = cart.filter(product => product.key !== key);
-        setCart(newCart);
-        removeFromDb(key);
-    }
+  const [products] = useProducts();
+  const [cart, setCart] = useCart(products);
+  const history = useHistory();
+  const handleRemove = (key) => {
+    const newCart = cart.filter((product) => product.key !== key);
+    setCart(newCart);
+    removeFromDb(key);
+  };
 
-    const handlePlaceholder = () =>
-    {
-        history.push('/placeorder');
-        setCart([]);
-        clearTheCart()
-        }
+  const handlePlaceholder = () => {
+    history.push("/placeorder");
+    setCart([]);
+    clearTheCart();
+  };
 
-    return (
-        <div className="shop-container">
-            <div className="product-container">
-                {
-                    cart.map(product => <ReviewItem
-                        key={product.key}
-                        product={product}
-                        handleRemove={handleRemove}
-                    ></ReviewItem>)
-                }
-            </div>
-            <div className="cart-container">
-                <Cart cart={ cart }>
-                    <h3> hello my world, sweet heart!!</h3>
-                    <button onClick={handlePlaceholder} className="btn-regular">placeholder</button>
-                </Cart>
-            </div>
-        </div>
-    );
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        {cart.map((product) => (
+          <ReviewItem
+            key={product.key}
+            product={product}
+            handleRemove={handleRemove}
+          ></ReviewItem>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}>
+          <button onClick={handlePlaceholder} className="btn-regular">
+            placeholder
+          </button>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default OrderReview;
